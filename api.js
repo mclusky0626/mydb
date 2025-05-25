6a21e66300086b46a4f1515b119af767e2c9fcb2
배건우 <102368729+mclusky0626@users.noreply.github.com>
2025-05-25 15:26:23 +0900

    테스트로 업데이트함
    제발

--- api.js
+++ api.js
@@ -1,6 +1,7 @@
 const { chemicalEquationBalancer } = require('chemical-equation-balancer-haizuka');
 
-const cbe = chemicalEquationBalancer("CaCo3 + H2O = Ca2 + H + O");
+const cbe = chemicalEquationBalancer("");
 const textResult = cbe.text;
 
 console.log(textResult); // 2H2 + O2 = 2H2O
+//test입니다만ㅇ내ㅔㅓㅐ
